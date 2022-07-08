097e9701a432b476c0b05b1c5277d5971307bcd8
quananle <leanhquan31198@gmail.com>
2022-07-08 18:04:39 +0700

    check in - inittalization facebook SDK

--- app/src/main/java/com/leaquan/petinder/ui/check_in/login/LoginFragment.kt
+++ app/src/main/java/com/leaquan/petinder/ui/check_in/login/LoginFragment.kt
@@ -1,7 +1,15 @@
 package com.leaquan.petinder.ui.check_in.login
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.os.bundleOf
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -10,12 +18,16 @@ import com.leaquan.petinder.base.fragment.BaseFragmentMVVM
 import com.leaquan.petinder.databinding.FragmentLoginBinding
 import com.leaquan.petinder.ui.check_in.forgot_password.ForgotPasswordFragment
 import com.leaquan.petinder.ui.check_in.register.RegisterFragment
-import com.leaquan.petinder.util.view_extension.transformation
-import com.leaquan.petinder.util.viewmodel.kodeinViewModel
+import com.leaquan.petinder.ui.home.HomeActivity
+import com.leaquan.petinder.util.Social
 import com.leaquan.petinder.util.extension.WTF
 import com.leaquan.petinder.util.extension.onClick
 import com.leaquan.petinder.util.pushFragment
 import com.leaquan.petinder.util.text.gradientText
+import com.leaquan.petinder.util.view_extension.transformation
+import com.leaquan.petinder.util.viewmodel.kodeinViewModel
+import java.util.*
+
 
 class LoginFragment : BaseFragmentMVVM<FragmentLoginBinding, LoginViewModel>() {
 
@@ -39,9 +51,12 @@ class LoginFragment : BaseFragmentMVVM<FragmentLoginBinding, LoginViewModel>() {
 
     private lateinit var auth : FirebaseAuth
 
+    private lateinit var callbackManager : CallbackManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = Firebase.auth
+        callbackManager = CallbackManager.Factory.create()
     }
 
     override fun onStart() {
@@ -66,7 +81,8 @@ class LoginFragment : BaseFragmentMVVM<FragmentLoginBinding, LoginViewModel>() {
             }
 
             btnLogin.onClick {
-              //  ToastPET(activity).makeText(activity,  "Đã chia sẽ cho thầy!", LONG, SUCCESS)?.show()
+                val intent = Intent(activity , HomeActivity::class.java)
+                startActivity(intent)
             }
 
             btnRegister.onClick {
@@ -76,20 +92,70 @@ class LoginFragment : BaseFragmentMVVM<FragmentLoginBinding, LoginViewModel>() {
             }
 
             layoutSocial.btnFacebook.onClick {
+                layoutSocial.loginFacebook.performClick()
+                loginWithFaceBook()
+                WTF("click roi mi")
             }
 
             layoutSocial.btnGoogle.onClick {
-
                 //authGoogle()
             }
 
             layoutSocial.btnPhone.onClick {
-                activity?.pushFragment(ForgotPasswordFragment())
+                activity?.pushFragment(
+                    ForgotPasswordFragment()
+                )
             }
         }
     }
 
-//    private fun authGoogle() {
+    private fun loginWithFaceBook() {
+        with(binding.layoutSocial) {
+            loginFacebook.setPermissions(Social.facebook_permissions)
+
+
+            // Callback registration
+            // If you are using in a fragment, call loginButton.setFragment(this);
+
+            // Callback registration
+            loginFacebook.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+
+                override fun onCancel() {
+                    WTF("facebook: cancel")
+                }
+
+                override fun onError(error: FacebookException) {
+                    WTF("facebook:onError  $error")
+                }
+
+                override fun onSuccess(result: LoginResult) {
+                    WTF("facebook:onSuccess:$result")
+                    handleFacebookAccessToken(result.accessToken)
+                    resultActivity.contract.apply {
+
+                    }
+                }
+            })
+
+        }
+    }
+
+    private val resultActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+        }
+    }
+
+    private fun handleFacebookAccessToken(accessToken: AccessToken) {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun setUpObserver() {
+        //    private fun authGoogle() {
 //        val googleCredential = oneTapClient.getSignInCredentialFromIntent(data)
 //        val idToken = googleCredential.googleIdToken
 //        when {
@@ -117,7 +183,7 @@ class LoginFragment : BaseFragmentMVVM<FragmentLoginBinding, LoginViewModel>() {
 //            }
 //        }
 //    }
-
-    override fun setUpObserver() {
     }
+
+
 }
